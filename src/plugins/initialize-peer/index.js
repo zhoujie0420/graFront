47bf671d185139b632d48d9a55538b4c9a76115f
jiezhou <1747364257@qq.com>
2024-02-18 11:30:08 +0800

    add record

--- src/plugins/initialize-peer/index.js
+++ src/plugins/initialize-peer/index.js
@@ -2,7 +2,11 @@ import Peer from "peerjs";
 import networkConfiguration from "@/network/configuration";
 import usePeerStore from "@/store/peer";
 import {showToast} from "vant";
+import $ from "jquery";
+import {apiUrl} from "../../../config";
 
+
+const user = usePeerStore();
 export default function initializePeer (id) {
     let peerStore = usePeerStore();
 
@@ -51,6 +55,7 @@ export default function initializePeer (id) {
 
     localPeer.on("call", mediaConnection => {
         console.log("localPeer call", mediaConnection);
+        addRecord(mediaConnection.peer,user.localPeer.id);
         peerStore.mediaConnection = mediaConnection;
     });
 
@@ -69,4 +74,28 @@ export default function initializePeer (id) {
         console.log("localPeer opened,the local peer id is: ", localPeerId);
         peerStore.localPeer = localPeer;
     });
+}
+
+function addRecord(id1,id2) {
+    $.ajax({
+        url: `${apiUrl}/api/record/addRecord/`,
+        type: "post",
+        data: {
+            userId1: id1,
+            userId2: id2,
+        },
+        headers: {
+            Authorization: "Bearer " + user.token,
+        },
+        success(resp) {
+            if (resp.code === 200) {
+                console.log(resp.message)
+            } else {
+                console.log(resp.message)
+            }
+        },
+        error(resp) {
+            console.log(resp)
+        }
+    })
 }
